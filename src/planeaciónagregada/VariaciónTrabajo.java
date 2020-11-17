b1444b05f81fa5da3f554a7551be208f6dc4ed3d
josej <josejesusarreola98@gmail.com>
2020-11-17 14:19:08 -0600

    Updated

---
+++ src/planeaciónagregada/VariaciónTrabajo.java
@@ -0,0 +1,411 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package planeaciónagregada;
+
+/**
+ *
+ * @author josej
+ */
+public class VariaciónTrabajo {
+    int P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12;
+    int PD1, PD2, PD3, PD4, PD5, PD6, PD7, PD8, PD9, PD10, PD11, PD12;
+    int PH1, PH2, PH3, PH4, PH5, PH6, PH7, PH8, PH9, PH10, PH11, PH12;
+    int InventarioInicial, InventarioSeguridad, HoraTrabajo, costoContratacion, CostoDespido, TiempoRegular;
+
+    public VariaciónTrabajo(int P1, int P2, int P3, int P4, int P5, int P6, int P7, int P8, int P9, int P10, int P11, int P12, int PD1, int PD2, int PD3, int PD4, int PD5, int PD6, int PD7, int PD8, int PD9, int PD10, int PD11, int PD12, int PH1, int PH2, int PH3, int PH4, int PH5, int PH6, int PH7, int PH8, int PH9, int PH10, int PH11, int PH12, int InventarioInicial, int InventarioSeguridad, int HoraTrabajo, int costoContratacion, int CostoDespido, int TiempoRegular) {
+        this.P1 = P1;
+        this.P2 = P2;
+        this.P3 = P3;
+        this.P4 = P4;
+        this.P5 = P5;
+        this.P6 = P6;
+        this.P7 = P7;
+        this.P8 = P8;
+        this.P9 = P9;
+        this.P10 = P10;
+        this.P11 = P11;
+        this.P12 = P12;
+        this.PD1 = PD1;
+        this.PD2 = PD2;
+        this.PD3 = PD3;
+        this.PD4 = PD4;
+        this.PD5 = PD5;
+        this.PD6 = PD6;
+        this.PD7 = PD7;
+        this.PD8 = PD8;
+        this.PD9 = PD9;
+        this.PD10 = PD10;
+        this.PD11 = PD11;
+        this.PD12 = PD12;
+        this.PH1 = PH1;
+        this.PH2 = PH2;
+        this.PH3 = PH3;
+        this.PH4 = PH4;
+        this.PH5 = PH5;
+        this.PH6 = PH6;
+        this.PH7 = PH7;
+        this.PH8 = PH8;
+        this.PH9 = PH9;
+        this.PH10 = PH10;
+        this.PH11 = PH11;
+        this.PH12 = PH12;
+        this.InventarioInicial = InventarioInicial;
+        this.InventarioSeguridad = InventarioSeguridad;
+        this.HoraTrabajo = HoraTrabajo;
+        this.costoContratacion = costoContratacion;
+        this.CostoDespido = CostoDespido;
+        this.TiempoRegular = TiempoRegular;
+    }
+
+    
+    
+    public int OperaVT(){
+        return 0;
+    }
+            
+
+
+
+    public int getP1() {
+        return P1;
+    }
+
+    public void setP1(int P1) {
+        this.P1 = P1;
+    }
+
+    public int getP2() {
+        return P2;
+    }
+
+    public void setP2(int P2) {
+        this.P2 = P2;
+    }
+
+    public int getP3() {
+        return P3;
+    }
+
+    public void setP3(int P3) {
+        this.P3 = P3;
+    }
+
+    public int getP4() {
+        return P4;
+    }
+
+    public void setP4(int P4) {
+        this.P4 = P4;
+    }
+
+    public int getP5() {
+        return P5;
+    }
+
+    public void setP5(int P5) {
+        this.P5 = P5;
+    }
+
+    public int getP6() {
+        return P6;
+    }
+
+    public void setP6(int P6) {
+        this.P6 = P6;
+    }
+
+    public int getP7() {
+        return P7;
+    }
+
+    public void setP7(int P7) {
+        this.P7 = P7;
+    }
+
+    public int getP8() {
+        return P8;
+    }
+
+    public void setP8(int P8) {
+        this.P8 = P8;
+    }
+
+    public int getP9() {
+        return P9;
+    }
+
+    public void setP9(int P9) {
+        this.P9 = P9;
+    }
+
+    public int getP10() {
+        return P10;
+    }
+
+    public void setP10(int P10) {
+        this.P10 = P10;
+    }
+
+    public int getP11() {
+        return P11;
+    }
+
+    public void setP11(int P11) {
+        this.P11 = P11;
+    }
+
+    public int getP12() {
+        return P12;
+    }
+
+    public void setP12(int P12) {
+        this.P12 = P12;
+    }
+
+    public int getPD1() {
+        return PD1;
+    }
+
+    public void setPD1(int PD1) {
+        this.PD1 = PD1;
+    }
+
+    public int getPD2() {
+        return PD2;
+    }
+
+    public void setPD2(int PD2) {
+        this.PD2 = PD2;
+    }
+
+    public int getPD3() {
+        return PD3;
+    }
+
+    public void setPD3(int PD3) {
+        this.PD3 = PD3;
+    }
+
+    public int getPD4() {
+        return PD4;
+    }
+
+    public void setPD4(int PD4) {
+        this.PD4 = PD4;
+    }
+
+    public int getPD5() {
+        return PD5;
+    }
+
+    public void setPD5(int PD5) {
+        this.PD5 = PD5;
+    }
+
+    public int getPD6() {
+        return PD6;
+    }
+
+    public void setPD6(int PD6) {
+        this.PD6 = PD6;
+    }
+
+    public int getPD7() {
+        return PD7;
+    }
+
+    public void setPD7(int PD7) {
+        this.PD7 = PD7;
+    }
+
+    public int getPD8() {
+        return PD8;
+    }
+
+    public void setPD8(int PD8) {
+        this.PD8 = PD8;
+    }
+
+    public int getPD9() {
+        return PD9;
+    }
+
+    public void setPD9(int PD9) {
+        this.PD9 = PD9;
+    }
+
+    public int getPD10() {
+        return PD10;
+    }
+
+    public void setPD10(int PD10) {
+        this.PD10 = PD10;
+    }
+
+    public int getPD11() {
+        return PD11;
+    }
+
+    public void setPD11(int PD11) {
+        this.PD11 = PD11;
+    }
+
+    public int getPD12() {
+        return PD12;
+    }
+
+    public void setPD12(int PD12) {
+        this.PD12 = PD12;
+    }
+
+    public int getPH1() {
+        return PH1;
+    }
+
+    public void setPH1(int PH1) {
+        this.PH1 = PH1;
+    }
+
+    public int getPH2() {
+        return PH2;
+    }
+
+    public void setPH2(int PH2) {
+        this.PH2 = PH2;
+    }
+
+    public int getPH3() {
+        return PH3;
+    }
+
+    public void setPH3(int PH3) {
+        this.PH3 = PH3;
+    }
+
+    public int getPH4() {
+        return PH4;
+    }
+
+    public void setPH4(int PH4) {
+        this.PH4 = PH4;
+    }
+
+    public int getPH5() {
+        return PH5;
+    }
+
+    public void setPH5(int PH5) {
+        this.PH5 = PH5;
+    }
+
+    public int getPH6() {
+        return PH6;
+    }
+
+    public void setPH6(int PH6) {
+        this.PH6 = PH6;
+    }
+
+    public int getPH7() {
+        return PH7;
+    }
+
+    public void setPH7(int PH7) {
+        this.PH7 = PH7;
+    }
+
+    public int getPH8() {
+        return PH8;
+    }
+
+    public void setPH8(int PH8) {
+        this.PH8 = PH8;
+    }
+
+    public int getPH9() {
+        return PH9;
+    }
+
+    public void setPH9(int PH9) {
+        this.PH9 = PH9;
+    }
+
+    public int getPH10() {
+        return PH10;
+    }
+
+    public void setPH10(int PH10) {
+        this.PH10 = PH10;
+    }
+
+    public int getPH11() {
+        return PH11;
+    }
+
+    public void setPH11(int PH11) {
+        this.PH11 = PH11;
+    }
+
+    public int getPH12() {
+        return PH12;
+    }
+
+    public void setPH12(int PH12) {
+        this.PH12 = PH12;
+    }
+
+    public int getInventarioInicial() {
+        return InventarioInicial;
+    }
+
+    public void setInventarioInicial(int InventarioInicial) {
+        this.InventarioInicial = InventarioInicial;
+    }
+
+    public int getInventarioSeguridad() {
+        return InventarioSeguridad;
+    }
+
+    public void setInventarioSeguridad(int InventarioSeguridad) {
+        this.InventarioSeguridad = InventarioSeguridad;
+    }
+
+    public int getHoraTrabajo() {
+        return HoraTrabajo;
+    }
+
+    public void setHoraTrabajo(int HoraTrabajo) {
+        this.HoraTrabajo = HoraTrabajo;
+    }
+
+    public int getCostoContratacion() {
+        return costoContratacion;
+    }
+
+    public void setCostoContratacion(int costoContratacion) {
+        this.costoContratacion = costoContratacion;
+    }
+
+    public int getCostoDespido() {
+        return CostoDespido;
+    }
+
+    public void setCostoDespido(int CostoDespido) {
+        this.CostoDespido = CostoDespido;
+    }
+
+    public int getTiempoRegular() {
+        return TiempoRegular;
+    }
+
+    public void setTiempoRegular(int TiempoRegular) {
+        this.TiempoRegular = TiempoRegular;
+    }
+
+
+
+
+}
