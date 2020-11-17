616be7d4de1133c21fc4532af2d6423e32c7a4ae
josej <josejesusarreola98@gmail.com>
2020-11-17 14:16:00 -0600

    Updated

---
+++ src/planeaciónagregada/PlaneaciónAgregada.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package planeaciónagregada;
+
+import Interfaz.PA;
+
+/**
+ *
+ * @author josej
+ */
+public class PlaneaciónAgregada {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PA s=new PA();
+        s.setVisible(true);
+    }
+    
+}
